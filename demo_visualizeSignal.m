108a3e7de8bec129f6d5034ec35aa9f5b8ae878b
Michael Mueller <michael.mueller@selfnet.de>
2014-07-06 23:13:23 +0200

    lets see if that works now

--- demo_visualizeSignal.m
+++ demo_visualizeSignal.m
@@ -4,6 +4,47 @@
 
 function demo_visualizeSignal(visual,predefined,predefsignal,uploadfile,filename)
 
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % BEGIN, set up graphical output
+
+    % Create an invisible figure.
+    if (webdemo_en)
+      fig = figure(1); set(fig, "visible", "off");
+    else
+      fig = figure(1); set(fig, "visible", "on");
+    endif
+
+    landscape = "-S930,350";
+    portrait  = "-S640,480";
+
+    output_format = landscape; %portrait;     % default
+
+
+    set(0, "defaultlinelinewidth", 2);
+
+    if (webdemo_en)
+      set(0, "defaultaxesfontsize", 12);
+      set(0, "defaultaxesfontname", "Arial");
+      set(0, "defaulttextfontsize", 12);
+      set(0, "defaulttextfontname", "Arial");
+      const_lw = 2;
+    elseif (svg_en)
+      set(0, "defaultaxesfontsize", 6);
+      set(0, "defaultaxesfontname", "Arial");
+      set(0, "defaulttextfontsize", 6);
+      set(0, "defaulttextfontname", "Arial");
+      const_lw = 1;
+    else
+      set(0, "defaultaxesfontsize", 10);
+      set(0, "defaultaxesfontname", "Arial");
+      set(0, "defaulttextfontsize", 8);
+      set(0, "defaulttextfontname", "Arial");
+      const_lw = 2;
+    endif
+
+    % END, set up graphical output
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % BEGIN prepare parameters for array
@@ -38,8 +79,19 @@ function demo_visualizeSignal(visual,predefined,predefsignal,uploadfile,filename
     % combine commands to modulearray
     ModuleArray = [filter; vis];
     
+     % END prepare parameters for array
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+   
+    
     visualizeSignal(inputFileLocation,1,ModuleArray);
     
+    
+    print(filname_output, "-dgif","-S640,480", "-r0");
+
+    if (svg_en)
+      print(["fig_",filname_output], "-dsvg",output_format);
+    endif
+
 end
 % END main function
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
